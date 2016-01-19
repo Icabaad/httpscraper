c2c3f75dadb5bbf7feb21030f6d8cc69d3d929d3
TheDanger <github@fallenreich.com>
2016-01-19 23:14:40 +1100

    Date conversion to mysql standard Insertion into db of values auto creation of columns still to go

---
+++ JSONfileread.py
@@ -0,0 +1,72 @@
+import json
+import datetime
+from dateutil.parser import parse
+import MySQLdb
+import os
+import httplib
+
+#EMONCMS details
+homedomain = "dangerproxy:8080"
+apikey = 'da3ae5f01b1245c3360ef85ddd8fb451'
+payload = 'string'
+conn = httplib.HTTPConnection(homedomain)
+
+db = MySQLdb.connect("192.168.0.2","SensorNet","netsensor","SensorDB")
+cursor = db.cursor()
+keynames = []
+trial = []
+playcount = 0
+file_object = open('//dangernas/Public/Scrapes/Lastfm/lastfmplay.txt', "rb")
+
+
+#textstr = file.readline()
+lines = file_object.read( ).splitlines(  )
+file_object.close()
+os.remove('//dangernas/Public/Scrapes/Lastfm/lastfmplay.txt')
+#print(lines)
+#print()
+
+for line in lines:
+    line = line.rstrip( )
+    line = line.rstrip('/n')
+    trial.append(json.loads(line))
+    #print(trial)
+#print (keynames)
+
+# grabs date from dictionary and converts to MySQL format. Yes!
+for x in trial:
+    dt = parse(x.get('Date'))
+    x['Date'] = (dt.strftime('%Y/%m/%d %H:%M:%S'));
+    #print(x)
+    keys = (x.keys())
+    values = (x.values())
+    sql = "INSERT INTO testtbl(%s, %s) VALUES ('%s', %s)" % (keys[0], keys[1], values[0], values[1]) #, values[1])
+    #print(sql)
+    playcount += 1
+    print (playcount)
+    try:
+     # Execute the SQL command
+        cursor.execute(sql)
+     # Commit your changes in the database
+        db.commit()
+    except:
+        print('No Jooce!!!')
+        sql2 = "ALTER TABLE testtbl ADD %s INT;" % (keys[1]) #, keys[1])
+        print(sql2)
+        cursor.execute(sql2)
+        cursor.execute(sql)
+        db.commit()
+    else:
+        db.rollback()
+
+    payload = '{' + "TracksPlayed:"+(str(playcount)) + '}'
+
+print (payload)
+try:
+    conn.request("GET", "/emoncms/input/post.json?node=1&apikey=" + apikey + "&json=" + payload)
+    response = conn.getresponse()
+    conn.close()
+    playcount = 0
+except (httplib.HTTPException, socket.error) as ex:
+    print "Error: %s" % ex
+
